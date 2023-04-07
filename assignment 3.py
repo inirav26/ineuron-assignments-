0c6e6aa92c717156ef4864a23cf6eb245803a9b6
inirav26 <113454409+inirav26@users.noreply.github.com>
2023-04-07 17:06:36 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,54 @@
+##1 
+## because of function duplicate code for programme is reduced nd with the help of function programme is shorter and easy to read
+
+
+##2 
+# when the function is called,  code in that function runs. 
+
+##3 
+
+def 
+
+##4 
+
+## function consist def and perticuler code for that. while we have to execute that function .that is function call. 
+
+##5 
+
+## only one global variable and local variables are created whenever we creates function . 
+
+##6 
+
+## when retuen is called in function , local variables are erased or we can say that deleted.. 
+
+##7 
+
+## return value is the value that a function call evaluates . return value can be used as part of an expression.
+
+##8 
+
+## value is None type 
+
+##9 
+
+## A global statement will force a variable in a function 
+
+##10 
+
+## nonetype data . 
+
+##11 
+
+## import from module areallyourpetsnamederic
+
+##12 
+
+#    spam.bacon()
+
+#13
+
+## put that code in try block 
+
+##14
+
+##  to save programme from crashing to encounter error . if error occurs which is handled by except block . 
