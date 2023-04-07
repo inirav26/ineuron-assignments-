365f78b482bc14b179b6dcc6977ee5f04f6b1dc2
inirav26 <113454409+inirav26@users.noreply.github.com>
2023-04-07 16:24:10 +0530

    Add files via upload

---
+++ assingment 2.py
@@ -0,0 +1,118 @@
+##1 
+# twp values pf boolean data types are 
+# 1. True 
+# 2. False
+
+# value  = True 
+# value = False 
+
+## 2 
+
+#diffrent boolean operators are 
+# 1. or 
+# 2.not
+# 3. and
+
+##3 
+x=  1
+y=  0
+print (bool(x))
+print (bool(y))
+
+##4 
+
+a= 5>4
+b= 3==5 
+print (a and b)
+ ## False 
+ 
+print (not 5>4)
+## False
+
+print (not (5>4) or (3==5))
+## False  
+
+print (5>4) or (3==5)
+## True 
+
+
+print ( not False ) or ( not True )
+## true 
+
+print ( True and True ) and ( True == False )
+## True 
+
+##5
+
+## six comparision operators are 
+ # 1. ==  equals to 
+# 2  > greator than 
+# 3. < less than 
+# 4. =! nor equals to 
+# 5. >= greator equals to 
+# 6. < = less equals to 
+
+## 6 
+
+## equal to compares two value while assignment operator  stores value for perticuler variable. 
+
+# a = 12 ( ## assignment operator)
+# b = 12 
+
+# a==b ( ## equal to operator )
+
+
+##7 
+
+spam = 0
+if spam == 10:
+   print('eggs')
+if spam > 5:
+   print('bacon')
+else:
+   print('ham')
+   print('spam')
+   print('spam')
+
+## here value of spam is 0  . in followimg condition if spaam value is quals to 10 than jt shold be printed egg . if value of spaam is greator than 5 than it should be printed 5 other wise print ham , spam , spam . because value is 0 
+
+##8 
+
+if spam == 1:
+   print("hello")
+elif spam ==2:
+   print("howdy")
+else:
+  print("greetings")
+
+##9 
+
+### break 
+
+##10 
+
+## The break statement will move the execution outside loop 
+## The continue statement will move the execution to the start of the loop.
+
+##11 
+
+##there is not any diffrence 
+
+##12
+
+i= 1 
+
+for i in range(1, 11):
+     print(i)
+
+## b 
+
+while i <= 10 : 
+	print(i)
+	i = i+1 
+
+##13 
+
+spam.bacon()
+
+
